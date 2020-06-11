ff5d527e5240a2b57f36af9cf31cb9af4ea27f2c
Nurettin Onur YAKIT <onuryakit@gmail.com>
2020-06-11 14:29:57 +0200

    Initial project + dynamic routing.

---
+++ pages/videos/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Video Details {{$route.params.id}}</h2>Here
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style>
+</style>
---
+++ pages/videos/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Videos</h1>
+    <div v-for="video in videos" :key="video.id">{{video.name}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videos: [
+        {
+          id: 16,
+          name: "First video."
+        },
+        {
+          id: 17,
+          name: "Second video."
+        },
+        {
+          id: 20,
+          name: "Latest video."
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
